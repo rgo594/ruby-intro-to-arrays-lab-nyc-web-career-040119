83cb8c621a0313512e5f637f9945d2d21f5d942a
Rob O'Gara <rgo594@gmail.com>
2019-03-16 15:32:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -4,10 +4,15 @@ def instantiate_new_array(array)
 end
 
 def array_with_two_elements(array, a, b)
-  array =[a, b]
+  array = [a, b]
   return array
 end
 
 def first_element(array)
   array.first
 end
+
+def third_element(array)
+  array[2]
+end
+
